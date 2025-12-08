858686c8fc4320b7e9e6d3bc8106e948a3966996
K-den <kbreinholt@icloud.com>
2025-12-08 13:24:30 -0500

    ok its done

--- alien_fleet.py
+++ alien_fleet.py
@@ -52,12 +52,10 @@ class AlienFleet:
                alien_w, alien_h, fleet_h, fleet_w, x_offset, y_offset
           )
 
-          # Use fleet_w = 17, fleet_h = 15 for debugging vvvvvvv  vvvvvvv
-          self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
 
      def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
-         for col in range(fleet_w):
-             for row in range(fleet_h):
+         for row in range(fleet_h):
+             for col in range(fleet_w):
                  current_x = alien_w * col + x_offset
                  current_y = alien_h * row + y_offset
                  if col % 2 == 0 or row % 2 == 0:
@@ -93,28 +91,11 @@ class AlienFleet:
           return x_offset, y_offset
 
 
-     # WORKING AS INTENDED
      def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
-         """Calculates the fleet size so that it fits the screen"""
-         fleet_h = int(((screen_w/2)//alien_w) + 1)
-         fleet_w = int((screen_h//alien_h) - 1)
+          """Calculates the fleet size so that it fits the screen"""
 
-         if fleet_h % 2 == 0:
-              fleet_h -=1
-
-         else:
-              fleet_h -= 2
-
-         if fleet_w % 2 == 0:
-              fleet_w -= 1
-         else:
-              fleet_w -= 2
-
-         print(f"{fleet_w}, {fleet_h}")
-         return fleet_w, fleet_h
-
-     def _create_alien(self, current_y: int, current_x: int):
-          new_alien = Alien(self, current_y, current_x, side='right')
+          # padding (left/top/bottom)
+          padding = int(alien_w * 0.5)
 
           # usable horizontal space = left half minus horizontal padding on both sides
           left_half_w = screen_w // 2
@@ -154,6 +135,7 @@ class AlienFleet:
                     self.fleet_direction *= -1
                     break
 
+
      def _shift_fleet_right(self):
           for alien in self.fleet:
               alien.x += self.fleet_speed
@@ -169,20 +151,20 @@ class AlienFleet:
           for alien in self.fleet:
                alien.draw_alien()
 
+
      def check_collisions(self, other_group):
           """What to do when the alien fleet collides with a bullet or the hero ship."""
           return pygame.sprite.groupcollide(self.fleet, other_group, True, True)
 
 
      def check_fleet_right(self):
+          alien: Alien
           for alien in self.fleet:
                if alien.rect.right >= self.settings.screen_w:
                     return True
           return False
 
-     def check_destroyed_status(self):
-          """Returns should the fleet be entirely destroyed."""
-          return not self.fleet
 
-          
-     
+     def check_destroyed_status(self):
+          """Returns whether the fleet is entirely destroyed."""
+          return not self.fleet
